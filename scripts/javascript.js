7b67114cf369014cccb99efa2120720a979fe84d
Darya Murauyova <muravkina@yahoo.com>
2015-05-18 12:02:36 -0400

    corrected syntax errors

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function(){
 $("#button").on("click", function(){
   $("#button img").hide();
-  var round = new Round;
+  var round = new Round();
   round.blink();
 });
 var Round = function() {
@@ -10,8 +10,8 @@ var Round = function() {
   this.$arrayColors = $(".colors div");
   this.clickedArray = [];
   this.matched = false;
-  this.$arrayColors.on("click",this.click.bind(this))
-}
+  this.$arrayColors.on("click",this.click.bind(this));
+};
 Round.prototype.blink = function() {
   this.round += 1;
   $("#round h3").text("Round");
@@ -25,12 +25,12 @@ Round.prototype.blink = function() {
     setTimeout(function(){
       highlight(randomColor);
     }, (600*i));
-  }
+  };
   for (var i = 0; i < this.showingArray.length; i++){
-    var randomColor = this.showingArray[i]
+    var randomColor = this.showingArray[i];
     shine(randomColor, i);
   }
-}
+};
 Round.prototype.click = function(event) {
   var pressed = event.target;
   press(pressed);
@@ -39,7 +39,7 @@ Round.prototype.click = function(event) {
   if (this.matched) {
     this.next();
   }
- }
+ };
 
 Round.prototype.check = function() {
   for (var i = 0; i < this.clickedArray.length; i++) {
@@ -51,7 +51,6 @@ Round.prototype.check = function() {
       this.round = 1;
       $("#round h3").empty();
       $("#round p").empty();
-      console.log("you lost")
       break;
     }
     else if (this.showingArray.length === this.clickedArray.length && this.showingArray[(this.showingArray.length-1)] === this.clickedArray[(this.clickedArray.length-1)]) {
@@ -59,15 +58,15 @@ Round.prototype.check = function() {
       this.matched = true;
     }
   }
-}
+};
 Round.prototype.next = function() {
-  var boundBlink = this.blink.bind(this)
+  var boundBlink = this.blink.bind(this);
     setTimeout(boundBlink, 1000);
-}
+};
 Round.prototype.lost = function() {
-  var roundCompleteness = "\n You completed " + this.round + " round(s)"
+  var roundCompleteness = "\n You completed " + this.round + " round(s)";
   $(".message").append("Oops, you lost" + "<br>"+ roundCompleteness);
-  this.$arrayColors.off("click")
+  this.$arrayColors.off("click");
   $("#blur").show("fast");
   $("#message").show("slow");
   $(".close").on("click", function() {
@@ -76,35 +75,35 @@ Round.prototype.lost = function() {
   });
   $("#round").hide();
   $("#button img").show();
-}
+};
 
 var highlight = function(randomColor){
   if ($(randomColor).is("#green")) {
     $("#green").animate({"marginLeft" : "-=10px", "marginTop" : "-=10px"}, "fast");
-    $("#green").animate({"marginLeft" : "+=10px", "marginTop" : "+=10px"}, "fast")
+    $("#green").animate({"marginLeft" : "+=10px", "marginTop" : "+=10px"}, "fast");
   }
   else if ($(randomColor).is("#blue")) {
     $("#blue").animate({"left" : "+=10px", "marginTop" : "-=10px"}, "fast");
-    $("#blue").animate({"left" : "-=10px", "marginTop" : "+=10px"}, "fast")
+    $("#blue").animate({"left" : "-=10px", "marginTop" : "+=10px"}, "fast");
     }
   else if ($(randomColor).is("#red")) {
     $("#red").animate({"marginLeft" : "-=10px", "top" : "+=10px"}, "fast");
-    $("#red").animate({"marginLeft" : "+=10px", "top" : "-=10px"}, "fast")
+    $("#red").animate({"marginLeft" : "+=10px", "top" : "-=10px"}, "fast");
   }
   else if ($(randomColor).is("#yellow")){
-    $("#yellow").animate({"left" : "+=10px", "top" : "+=10px"}, "fast")
-    $("#yellow").animate({"left" : "-=10px", "top" : "-=10px"}, "fast")
+    $("#yellow").animate({"left" : "+=10px", "top" : "+=10px"}, "fast");
+    $("#yellow").animate({"left" : "-=10px", "top" : "-=10px"}, "fast");
   }
-}
+};
 var press = function(pressed) {
   $(pressed).animate({boxShadow: "-3px 3px 3px -1px rgba(100,100,100,1)"},50);
-  $(pressed).animate({boxShadow: "-3px 3px 6px 3px rgba(100,100,100,1)"}, 50)
-}
+  $(pressed).animate({boxShadow: "-3px 3px 6px 3px rgba(100,100,100,1)"}, 50);
+};
 
 $("#button").on("click",function(){
-  $("button").animate({boxShadow: "box-shadow: inset 0px 0px 18px 5px rgba(179,175,179,1);"})
-})
-})
+  $("button").animate({boxShadow: "box-shadow: inset 0px 0px 18px 5px rgba(179,175,179,1);"});
+});
+});
 
 
 
